6c26b788b18d5fa0789aa33c599efb0832e6fd07
Edwin Wallin <edwin.wallin@cgi.com>
2021-01-28 14:41:52 +0100

    Assignment and some expression ast nodes modelled

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <utility>
 
 namespace deq {
 	namespace ast {
@@ -26,33 +27,64 @@ namespace deq {
 
 		struct Node {
 			enum Type {
-				DECL_VAR
+				DECL_VAR,
+
+				OP_EXPR,
+				LIT_EXPR,
+				ASS_EXPR
 			};
 
 			Type type;
+
+			Node(Type type) : type(type) {};
 		};
 
 		struct ExpressionNode : public Node {
+			ExpressionNode(Node::Type type) : Node(type) {};
 		};
 
 		struct OperatorNode : public ExpressionNode {
 			std::string op;
 			std::unique_ptr<ExpressionNode> left;
 			std::unique_ptr<ExpressionNode> right;
+
+			OperatorNode(std::unique_ptr<ExpressionNode> left, std::unique_ptr<ExpressionNode> right, std::string op) 
+				:	ExpressionNode(Node::OP_EXPR), 
+					left(std::move(left)),
+					right(std::move(right)),
+					op(op)
+				{};
 		};
 
 		struct LiteralNode : public ExpressionNode {
 			std::string value;
+
+			LiteralNode(std::string value)
+				:	ExpressionNode(Node::LIT_EXPR),
+					value(value)
+				{};
 		};
 
-		struct AssignNode : public Node {
+		struct AssignNode : public ExpressionNode {
 			std::string name;
 			std::unique_ptr<ExpressionNode> value;
+
+			AssignNode(std::string name, std::unique_ptr<ExpressionNode> value)
+				:	ExpressionNode(Node::ASS_EXPR),
+					name(name),
+					value(std::move(value))
+				{};
 		};
 
-		struct VarDeclareNode : public Node 
-			VarType type;
+		struct VarDeclareNode : public Node {
+			VarType varType;
 			std::string name;
+
+			VarDeclareNode(VarType varType, std::string name)
+				:	Node(Node::DECL_VAR),
+					varType(varType),
+					name(name)
+				{};
 		};
 	}
 }
